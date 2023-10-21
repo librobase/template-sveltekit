f6e98b992d83d4e451abd97122b52ee7587b20c0
Mohammad Pishahang <64814209+mhpishahang@users.noreply.github.com>
2023-10-20 17:47:14 -0700

    add use/base64

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button>testing</button>
+<section class="p-3">
+  hey
+</section>
