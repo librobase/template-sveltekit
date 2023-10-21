7cdf7c8ebfb3c08ed21a6b728b96b34e0489da45
Mohammad Pishahang <64814209+mhpishahang@users.noreply.github.com>
2023-10-20 17:48:41 -0700

    add use/reactive

---
+++ src/lib/use/reactive.js
@@ -0,0 +1,41 @@
+import { writable, get } from 'svelte/store'
+
+// -----------------------------------------------------------------------------
+export function store(obj) {
+  // create a writable store
+  let { subscribe, set, update } = writable(obj)
+
+  // define a stringify function
+  let getObj = () => {
+    let obj = get({ subscribe })
+    return obj
+  }
+
+  let handler = {
+    get: (target, prop) => {
+      if (prop == 'subscribe') {
+        return subscribe
+      } else if (prop == 'getObj') {
+        return getObj
+      } else {
+        let obj = get({ subscribe })
+        return obj[prop]
+      }
+    },
+    set: (target, prop, value) => {
+      let obj = get({ subscribe })
+      obj[prop] = value
+      set(obj)
+      return true
+    }
+  }
+
+  let newStore = new Proxy({ subscribe, getObj }, handler)
+
+  return newStore
+}
+
+// -----------------------------------------------------------------------------
+export default {
+  store
+}
